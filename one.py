4d576af4fa2deeff1492fbeb51eb19565499a92f
jackson lu <jlu080@ucr.edu>
2020-05-31 12:02:53 -0700

    Removed maillog

--- one.py
+++ one.py
@@ -1,15 +1,22 @@
 import sys
 import re
 
-filename = str(sys.argv[1])
-textfile = open(filename, "r")
-filetext = textfile.read()
-textfile.close()
-matches = re.findall("rejected",filetext)
-
-a = 0
-for match in matches:
-	a += 1
+filename = sys.argv[1]
+num_reject = 0
+num_quarantine = 0
+with open(filename) as fp:
+	line = fp.readline()
+	match = re.search(.*(([A-Z][a-z][a-z]{1})([\s]*[1]{1}[\s]*)..:([0][0]):([0-5][0-9])).*/g,line)
+	if match:
+		selected_string = match.string()
+		if "reject" in selected_string:
+			num_reject += 1
+		elif "quarantine" in selected_string:
+			num_quarantine += 1
+		
+fp.closed
+print("Number of Rejects: " + num_reject)
+print("Number of Quarantine: " + num_quarantine)
 
 	
 
